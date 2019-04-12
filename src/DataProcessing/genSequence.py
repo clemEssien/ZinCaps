502c414c323814ace530ccf27d467424643a120f
clemEssien <clem.essien@gmail.com>
2019-04-12 17:42:36 -0500

    filter relevant dssp files

--- src/DataProcessing/genSequence.py
+++ src/DataProcessing/genSequence.py
@@ -1,7 +1,9 @@
 import os
+import shutil
 from glob import glob
 from collections import defaultdict
 import re
+from tqdm import tqdm
 
 def capitalise(str):
     return ">" + str[0:6].upper() + str[6:len(str)]
@@ -12,6 +14,12 @@ def f_comma(my_str, group, char):
 
 def index_of_xters_in_string(string, xter):
 	return [match.start() for match in re.finditer(re.escape(xter), string)]
+
+def move_file_to_folder(pdb_id, folder_old, folder_new):
+    for filename in os.listdir(folder_old):
+        filename = filename.split('.dssp')[0]
+        if filename.strip().lower() == pdb_id.strip().lower():
+            shutil.move(folder_old+"/"+filename+".dssp", folder_new+"/"+filename+".dssp")
 
 def insert(string, array,xter):
   sequence= [0];
@@ -25,6 +33,8 @@ def insert(string, array,xter):
 
 DATASET_DIR = "../../data/dataset/"
 RES_DIR = "../../data/resource/"
+DSSP_DIR_OLD = "../../data/resource/dssp"
+DSSP_DIR_NEW = "../../data/resource/dssp_for_use"
 positive_pids = []
 negative_pids = []
 temp_pos_list = []
@@ -56,42 +66,47 @@ positive_pids = list(set(positive_pids))
 negative_pids = list(set(negative_pids))
 combined_pids = list(set(positive_pids +negative_pids))
 
-print(pos_dict['3EB5_A'])
-print(type(pos_dict['3EB5_A']))
-print(neg_dict)
-with open(RES_DIR+"pdb_seqres.txt", "r") as file:
-   fasta_list = []
-   data = file.read()
-   seq_list = data.split(">")
+for i in tqdm(range(len(combined_pids))):
+    #print(combined_pids[i][0:4])
+    move_file_to_folder(combined_pids[i][0:4], DSSP_DIR_OLD, DSSP_DIR_NEW)
 
-   for pdb_id in positive_pids:
-       for seq in seq_list:
-           if(pdb_id.strip().lower() == seq[0:6].strip().lower()):
-               with open(RES_DIR+"pos_sequence.fasta", "a") as fasta_file:
-                    fasta_file.write(capitalise(seq))
+# print(pos_dict['3EB5_A'])
+# print(type(pos_dict['3EB5_A']))
+# print(neg_dict)
+# with open(RES_DIR+"pdb_seqres.txt", "r") as file:
+#    fasta_list = []
+#    data = file.read()
+#    seq_list = data.split(">")
+#
+#    for pdb_id in positive_pids:
+#        for seq in seq_list:
+#            if(pdb_id.strip().lower() == seq[0:6].strip().lower()):
+#                with open(RES_DIR+"pos_sequence.fasta", "a") as fasta_file:
+#                     fasta_file.write(capitalise(seq))
+#
+# # create positive dataset with annotations
+# with open(RES_DIR + "pos_sequence.fasta", "r") as file:
+#     data = file.read()
+#     seq_list = data.split(">")
+#     # print(seq_list[0])
+#     # print(seq_list[1])
+#     # print(seq_list[2])
+#     # print("-------------------")
+#     for i in range(len(seq_list)):
+#         pos_seq_list = seq_list[i].strip().split("\n")
+#         if len(pos_seq_list) == 2:
+#             pdb_id = pos_seq_list[0][0:6].strip()
+#             position_list = [int(x[0].split('+')[0]) for x in pos_dict[pdb_id]]  # in files positions start from 1
+#             #print(arr)
+#             #print(pos_seq_list[1])
+#             seq = insert(pos_seq_list[1].strip(),position_list,"#")
+#             print(pdb_id , position_list, seq)
+#             with open(RES_DIR + "pos_annotated_sequence.fasta", "a") as fasta_file:
+#                 fasta_file.write(">"+pdb_id+"\n"+seq)
+#             #print(pos_seq_list[1])
+#         # pdb_id = pos_seq_list[0][0:6]
+#         # seq = pos_seq_list[1]
+#         # print(seq)
+#
+#         # print(len(pos_seq_list))
 
-# create positive dataset with annotations
-with open(RES_DIR + "pos_sequence.fasta", "r") as file:
-    data = file.read()
-    seq_list = data.split(">")
-    # print(seq_list[0])
-    # print(seq_list[1])
-    # print(seq_list[2])
-    # print("-------------------")
-    for i in range(len(seq_list)):
-        pos_seq_list = seq_list[i].strip().split("\n")
-        if len(pos_seq_list) == 2:
-            pdb_id = pos_seq_list[0][0:6].strip()
-            position_list = [int(x[0].split('+')[0]) for x in pos_dict[pdb_id]]  # in files positions start from 1
-            #print(arr)
-            #print(pos_seq_list[1])
-            seq = insert(pos_seq_list[1].strip(),position_list,"#")
-            print(pdb_id , position_list, seq)
-            with open(RES_DIR + "pos_annotated_sequence.fasta", "a") as fasta_file:
-                fasta_file.write(">"+pdb_id+"\n"+seq)
-            #print(pos_seq_list[1])
-        # pdb_id = pos_seq_list[0][0:6]
-        # seq = pos_seq_list[1]
-        # print(seq)
-
-        # print(len(pos_seq_list))
