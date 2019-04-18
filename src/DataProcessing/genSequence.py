65146400a363c1b69141d0e4f57e7843ac479471
Clement <clem.essien@gmail.com>
2019-04-18 13:40:17 -0500

    temporarily removed 1GHX_L from negative dataset and charges from both datasets

--- src/DataProcessing/genSequence.py
+++ src/DataProcessing/genSequence.py
@@ -3,6 +3,9 @@ import shutil
 from glob import glob
 from collections import defaultdict
 import re
+import numpy as np
+from itertools import islice
+
 from tqdm import tqdm
 
 def capitalise(str):
@@ -13,7 +16,7 @@ def f_comma(my_str, group, char):
     return char.join(my_str[i:i+group] for i in range(0, len(my_str), group))
 
 def index_of_xters_in_string(string, xter):
-	return [match.start() for match in re.finditer(re.escape(xter), string)]
+   return [match.start() for match in re.finditer(re.escape(xter), string)]
 
 def move_file_to_folder(pdb_id, folder_old, folder_new):
     for filename in os.listdir(folder_old):
@@ -21,15 +24,44 @@ def move_file_to_folder(pdb_id, folder_old, folder_new):
         if filename.strip().lower() == pdb_id.strip().lower():
             shutil.move(folder_old+"/"+filename+".dssp", folder_new+"/"+filename+".dssp")
 
+def fileList(folder_old):
+    files = []
+    for filename in os.listdir(folder_old):
+        filename = filename.split('.dssp')[0]
+        files.append(filename)
+    return files
+
+
 def insert(string, array,xter):
   sequence= [0];
   new_string = "";
   i = 1;
   for i in range(len(array)):
-    sequence.append(array[i]+1) #seems the regphos position annotation counts from zero
+    sequence.append(array[i]) #seems the regphos position annotation counts from zero
     new_string += string[sequence[i]:sequence[i+1]]+xter
   new_string += string[sequence[-1]:]+"\n"
   return new_string
+
+
+def write_pos_annotations(pdb_id,seq):
+    #print(">"+pdb_id+"\n"+seq)
+    with open(RES_DIR + "pos_annotated_sequence.fasta", "a") as fasta_file:
+        fasta_file.write(">"+pdb_id+"\n"+seq)
+
+
+def parse_dssp_file(file_id, pos_dict):
+    print(pos_dict[file_id][0])
+    with open(DSSP_DIR_OLD + "/" + file_id[0:4] + ".dssp", "r") as file:
+        line_no = 0
+        seq = ""
+        for line in file:
+            line_no += 1
+            if line_no > 28 and line.split()[len(line.split())-1].strip() == file_id[5:6].strip().upper():
+                seq += line.split()[3]
+                pos = list(map(str, pos_dict[file_id][0]))
+                if line.split()[1] in pos:
+                    seq += "#"
+    write_pos_annotations(file_id,seq)
 
 DATASET_DIR = "../../data/dataset/"
 RES_DIR = "../../data/resource/"
@@ -50,15 +82,17 @@ for files in glob(DATASET_DIR+"*"):
             id = f[0:6]
             #print(id +" "+f[11:14])
             positive_pids.append(id);
-            if(positive_pids[len(positive_pids)-1] == id):
+            if positive_pids[len(positive_pids)-1] == id :
                 temp_pos_list.append(f[11:14].strip())
+            temp_pos_list.sort()
             pos_dict[id].append(temp_pos_list)
             temp_pos_list = []
         else:
             id = f[0:6]
             negative_pids.append(id);
-            if(negative_pids[len(negative_pids)-1] == id):
+            if negative_pids[len(negative_pids)-1] == id:
                 temp_neg_list.append(f[11:14].strip())
+            temp_neg_list.sort()
             neg_dict[id].append(temp_neg_list)
             temp_neg_list = []
 
@@ -66,9 +100,25 @@ positive_pids = list(set(positive_pids))
 negative_pids = list(set(negative_pids))
 combined_pids = list(set(positive_pids +negative_pids))
 
-for i in tqdm(range(len(combined_pids))):
-    #print(combined_pids[i][0:4])
-    move_file_to_folder(combined_pids[i][0:4], DSSP_DIR_OLD, DSSP_DIR_NEW)
+
+#positive annotations
+for i in range(len(positive_pids)):
+    parse_dssp_file(positive_pids[i], pos_dict)
+
+# for i in range(len(positive_pids)):
+#     file_id = positive_pids[i][0:4].lower()
+#     print(file_id)
+# #print(combined_pids)
+
+
+
+
+#files = fileList(DSSP_DIR_OLD)
+
+# for i in tqdm(range(len(combined_pids))):
+#     print(combined_pids[i][0:4])
+#     move_file_to_folder(combined_pids[i][0:4], DSSP_DIR_OLD, DSSP_DIR_NEW)
+
 
 # print(pos_dict['3EB5_A'])
 # print(type(pos_dict['3EB5_A']))
@@ -109,4 +159,3 @@ for i in tqdm(range(len(combined_pids))):
 #         # print(seq)
 #
 #         # print(len(pos_seq_list))
-
